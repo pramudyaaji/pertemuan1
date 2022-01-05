2befdf3512c1db5d37e41a46b26f81c3d086129e
Pramudya Aji <76149321+pramudyaaji@users.noreply.github.com>
2022-01-05 13:38:42 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <title>Latihan no 1</title>
+</head>
+<body>
+    <!-- Tag P  -->
+<p>Nama : Pramudya Restu Aji<br>
+ NIM : 5190311123
+</p>
+<p>Ini Paragraph</p>
+<br>
+<!-- Heading -->
+<h1>heading1</h1>
+<h2>heading2</h2>
+<h3>heading3</h3>
+<h4>heading4</h4>
+<h5>heading5</h5>
+<h6>heading6</h6>
+<br>
+
+
+        Harmony » Blog » Business
+
+Pengertian Business Intelligence (BI) Dan Manfaatnya Untuk Bisnis Anda
+
+</body>
+</html>
---
+++ tugasloginbootstrap.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Form LOGIN</title>
+</head>
+<div class="position-absolute top-50 start-50 translate-middle">
+<body class="p-3 mb-2 bg-success text-black">
+<div class="mx-auto" style="width: 400px;">
+<div class="w-100 p-3" style="background-color: #eee;">
+<h3 class="text-center ">LOGIN</h3>
+    <div class="container" >
+    <form  >
+  <div class="form-group">
+    <label for="emailAdress">Email or Username</label>
+    <input type="text" class="form-control" id="emailAdress" placeholder="Email or Username">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <br>
+  <div align="center">
+  <button type="submit" class="btn btn-dark" >Login</button>
+</div>
+</div>
+</form>
+</div>
+    </div>
+    </div>
+
+</body>
+</html>
